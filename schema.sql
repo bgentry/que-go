b91a13df1d923a6f5649ef16108d6b59c058d546
Corey Larson <corey.larson@getweave.com>
2019-11-07 13:47:37 -0700

    JSON -> JSONB

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS que_jobs
   run_at      timestamptz NOT NULL DEFAULT now(),
   job_id      bigserial   NOT NULL,
   job_class   text        NOT NULL,
-  args        json        NOT NULL DEFAULT '[]'::json,
+  args        jsonb       NOT NULL DEFAULT '[]'::jsonb,
   error_count integer     NOT NULL DEFAULT 0,
   last_error  text,
   queue       text        NOT NULL DEFAULT '',
