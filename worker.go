5bd7f23290c62b5255d02b5676017f659a4a0c84
saiteja2021 <90319810+saiteja2021@users.noreply.github.com>
2023-06-29 12:55:23 +0530

    STK-1038 (#11)

--- worker.go
+++ worker.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/jackc/pgx/v4"
 	"log"
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 	"weavelab.xyz/monorail/shared/wlib/wlog"
@@ -74,7 +76,8 @@ func NewWorker(c *Client, m WorkMap) *Worker {
 
 // Work pulls jobs off the Worker's Queue at its Interval. This function only
 // returns after Shutdown() is called, so it should be run in its own goroutine.
-func (w *Worker) Work(ctx context.Context) {
+func (w *Worker) Work(ctx context.Context, n int) {
+
 	for {
 		select {
 		case <-w.ch:
@@ -82,7 +85,7 @@ func (w *Worker) Work(ctx context.Context) {
 			return
 		case <-time.After(w.Interval):
 			for {
-				if didWork := w.WorkOne(ctx); !didWork {
+				if didWork := w.WorkOne(ctx, n); !didWork {
 					break // didn't do any work, go back to sleep
 				}
 			}
@@ -90,40 +93,143 @@ func (w *Worker) Work(ctx context.Context) {
 	}
 }
 
-func (w *Worker) WorkOne(ctx context.Context) (didWork bool) {
-	j, err := w.c.LockJob(ctx, w.Queue)
-	if err != nil {
-		log.Printf("attempting to lock job: %v", err)
-		return
-	}
-	if j == nil {
-		return // no job was available
-	}
-	defer j.Done(ctx)
-	defer recoverPanic(ctx, j)
-	didWork = true
-	j.WorkerID = w.ID
-	j.Client = w.c
-	wf, ok := w.m[j.Type]
-	if !ok {
-		msg := fmt.Sprintf("unknown job type: %q", j.Type)
-		log.Println(msg)
-		if err = j.Error(ctx, msg); err != nil {
-			log.Printf("attempting to save error on job %d: %v", j.ID, err)
+type Tx struct {
+	tx pgx.Tx
+	mu sync.Mutex
+}
+
+func (ct *Tx) Rollback(ctx context.Context) error {
+	ct.mu.Lock()
+	defer ct.mu.Unlock()
+
+	return ct.tx.Rollback(ctx)
+}
+
+func (ct *Tx) Commit(ctx context.Context) error {
+	ct.mu.Lock()
+	defer ct.mu.Unlock()
+
+	return ct.tx.Commit(ctx)
+}
+func (ct *Tx) QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row {
+	ct.mu.Lock()
+	defer ct.mu.Unlock()
+
+	return ct.tx.QueryRow(ctx, sql, args...)
+}
+func (ct *Tx) Exec(ctx context.Context, sql string, args ...interface{}) error {
+	ct.mu.Lock()
+	defer ct.mu.Unlock()
+
+	_, err := ct.tx.Exec(ctx, sql, args...)
+	return err
+}
+func (w *Worker) printAvailableDBCons(n int) {
+	for {
+		select {
+		case <-w.ch:
+			log.Println("worker done")
+			return
+		case <-time.After(time.Second * 5):
+			log.Printf("wroker %v acquired db connections %v, totol %v ,max %v", n, w.c.pool.Stat().AcquiredConns(), w.c.pool.Stat().TotalConns(),
+				w.c.pool.Stat().MaxConns())
 		}
-		return
 	}
+}
 
-	if err = wf(j); err != nil {
-		j.Error(ctx, err.Error())
-		return
-	}
+func (w *Worker) WorkOne(ctx context.Context, n int) (didWork bool) {
 
-	if err = j.Delete(ctx); err != nil {
-		log.Printf("attempting to delete job %d: %v", j.ID, err)
+	for i := 0; i < maxLockJobAttempts; i++ {
+		tx, err := w.c.pool.Begin(ctx)
+		if err != nil {
+			wlog.InfoC(ctx, fmt.Sprintf("unable to begin transaction: %v", err))
+			return
+		}
+		transaction := Tx{
+			tx: tx,
+		}
+		j := Job{}
+
+		err = transaction.QueryRow(ctx, sqlGlobalLockJob, w.Queue).Scan(
+			&j.Queue,
+			&j.Priority,
+			&j.RunAt,
+			&j.ID,
+			&j.Type,
+			&j.Args,
+			&j.ErrorCount,
+			&j.ShardID,
+			&j.LastError,
+		)
+
+		if err != nil {
+			err2 := transaction.Rollback(ctx)
+			if err2 != nil {
+				log.Printf("error while rolling back %v", err)
+			}
+			if strings.Contains(err.Error(), "no rows in result set") {
+				log.Printf("attempting to lock the job from wroker %v : %v", n, err)
+				return
+			} else {
+				log.Printf("attempt %v  to lock the job from wroker %v : %v", i+1, n, err)
+				continue
+			}
+		} else {
+			log.Printf("attempt %v from woker %v| sucessfully locked job : %v", i+1, n, j.ID)
+			job := &j
+
+			if job == nil || (job != nil && job.ID == 0) {
+				err2 := transaction.Rollback(ctx)
+				if err2 != nil {
+					log.Printf("error while rolling back %v", err)
+				}
+				return // no job was available
+			}
+			defer recoverPanic(ctx, job)
+			didWork = true
+			job.WorkerID = w.ID
+			job.Client = w.c
+
+			wf, ok := w.m[job.Type]
+			if !ok {
+				msg := fmt.Sprintf("unknown job type: %q", j.Type)
+				log.Println(msg)
+				if err = j.Error(ctx, msg); err != nil {
+					log.Printf("attempting to save error on job %d: %v", j.ID, err)
+				}
+				return
+			}
+
+			if err = wf(job); err != nil {
+				job.Error(ctx, err.Error())
+				return
+			}
+
+			err = transaction.Exec(ctx, sqlDeleteJob, j.Queue, j.Priority, j.RunAt, j.ID)
+			if err != nil {
+				err2 := transaction.Rollback(ctx)
+				if err2 != nil {
+					log.Printf("error while rolling back %v", err)
+				}
+
+				log.Printf("attempting to delete job %d: %v", j.ID, err)
+				return
+			}
+			err = transaction.Commit(ctx)
+			if err != nil {
+				err2 := transaction.Rollback(ctx)
+				if err2 != nil {
+					log.Printf("error while rolling back %v", err)
+				}
+				log.Printf("error while Committing changes  %v", err)
+
+			}
+			wlog.InfoC(ctx, fmt.Sprintf("wroker %v event is done =job_worked job_id=%d job_type=%s", n, j.ID, j.Type))
+
+			break
+		}
+
 	}
-	//
-	wlog.InfoC(ctx, fmt.Sprintf("event=job_worked job_id=%d job_type=%s", j.ID, j.Type))
 
 	return
 }
@@ -199,7 +305,8 @@ func (w *WorkerPool) Start(ctx context.Context) {
 		w.workers[i].Interval = w.Interval
 		w.workers[i].Queue = w.Queue
 		w.workers[i].ID = int(i)
-		go w.workers[i].Work(ctx)
+		go w.workers[i].Work(ctx, i)
+		//go w.workers[i].printAvailableDBCons(i)
 	}
 }
 
